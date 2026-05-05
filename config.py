c76285dabea815c5f31f1a389ab6833c2be4576e
amarshmellow <217641723+amarshmellow@users.noreply.github.com>
2026-05-05 20:27:54 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,3 @@
+# Device Customisation
+dmx_address = 0  # Our device Base DMX Address
+dmx_channels = 6   # How many DMX Channels will get passed to the callback function
---
+++ main.py
@@ -0,0 +1,147 @@
+from micropython import alloc_emergency_exception_buf
+import gc
+import dmx512_rx
+import config
+import LCD1602
+import ws2812
+import uasyncio
+import utime
+import machine
+import random
+
+channels = [0,0,0,0,0,0]
+
+
+candelcolour = (177,64,13)
+
+LED_PIN = const(17)
+LED_DUTY_CYCLE = const(5000)  # PWM rate, out of 65535
+LED_FREQUENCY = const(5000)  # PWM frequency, in Hz
+
+led = machine.PWM(machine.Pin(LED_PIN, machine.Pin.OUT))
+led.freq(LED_FREQUENCY)
+
+# mock class should the LCD not be detected
+class NoLcd:
+    def print_lcd(self, _m, _blank=True):
+        return
+    def setCursor(self, _x, _y):
+        return
+    def printout(self, _m):
+        return
+
+try:
+    lcd = LCD1602.LCD1602(16,2)
+except OSError:
+    lcd = NoLcd()
+
+brightness = 255
+
+gamma = 2.8
+brightness_lut = [int(((i / 255.0) ** gamma) * 255.0) for i in range(256)]
+
+def scale_color(rgb):
+    global brightness
+    return tuple(((c * brightness_lut[brightness])//255) for c in rgb)
+
+# Get DMX Configuration
+#dmxrx_deviceaddress = config.dmx_address  # Our device Base DMX Address
+#dmxrx_devicechannels = config.dmx_channels  # How many channels we care about
+
+# Environment Setup
+gc.threshold(16384)  # Run Garbage collection everytime 16KB is allocated
+alloc_emergency_exception_buf(512)  # Allocate Emergency Exception Buffer
+
+
+# def update(grgbw_list):
+#     global channels 
+#     lcd.print_lcd(" ".join(f"{value:03}" for value in grgbw_list[0:3])+"     "+" ".join(f"{value:03}" for value in grgbw_list[3:6]), False)
+#     channels = grgbw_list[0:6]
+# 
+# 
+# def dmxstatuschange(status):
+#     global channels
+# 
+#     if status == 0: # We are offline & timed-out
+#         print("Turning off LED Output")
+#         lcd.print_lcd("DISCONNECTED")
+#         for i in range(len(channels)): channels[i] = 0
+
+
+# Configuring Modules - DMX Receiver
+#dmx = dmx512_rx.DMX(dmxrx_deviceaddress, dmxrx_devicechannels, 1)
+#dmx.set_updatefunction(update)
+#dmx.set_statusfunction(dmxstatuschange)  # Not needed with full rainbow fallback
+
+async def blank():
+    ws2812.pixels_fill((0,0,0))
+    await ws2812.pixels_show()
+
+async def led_flash():
+    try:
+        print("flasher running")
+        start_time = utime.time()
+        while True:
+            while utime.time() < start_time + 1:
+                await uasyncio.sleep(0.05)
+            led.duty_u16(LED_DUTY_CYCLE)
+            await uasyncio.sleep(0.02)
+            led.duty_u16(0)
+            start_time += 3
+    except uasyncio.CancelledError:
+        pass
+
+
+    
+async def main():
+    global channels, brightness
+    currentpattern = None
+    currenttask = None
+    await blank()
+    while True:
+        #dmx.loop()
+        
+        chance = random.randint(0,100)
+        
+        if chance >= 98:
+
+            chance = random.randint(0,100)
+            
+            if chance > 50:
+                brightness = 100
+                ws2812.pixels_set(2,scale_color(candelcolour))
+                await ws2812.pixels_show()
+                await uasyncio.sleep(0.1)
+                
+                
+            else:
+                brightness = 50
+                ws2812.pixels_set(2,scale_color(candelcolour))
+                await ws2812.pixels_show()
+                await uasyncio.sleep(0.1)
+            
+        else:
+            brightness = 75
+            ws2812.pixels_set(2,scale_color(candelcolour))
+            await ws2812.pixels_show()
+        
+        
+        await uasyncio.sleep(0)
+        
+
+
+
+if __name__ == "__main__":
+    try:
+        #uasyncio.create_task(led_flash())
+        uasyncio.run(main())
+    except KeyboardInterrupt:
+        uasyncio.run(blank())
+        print("clearing screen")
+        lcd.print_lcd("")
+        utime.sleep(3)
+        print("exiting")
+
+
+if __name__ == "__main__":
+    uasyncio.run(main())
